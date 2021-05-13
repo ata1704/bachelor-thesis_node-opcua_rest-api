e42c3466e7dc4f96defe6ccc7ee98f65ce859157
marcelschalk <ata1704@gmail.com>
2021-05-13 14:37:02 +0200

    test

---
+++ app.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import logger from 'morgan';
+
+import apiRouter from './routes/api.js'
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use('/api', apiRouter);
+
+
+export default app;
